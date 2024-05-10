198ac22cbe19efecd0869387f94b51debb55796b
Maayan56 <maayansha12@gmail.com>
2024-05-10 22:14:52 +0300

    update maayan.java

--- maayan.java
+++ maayan.java
@@ -1,7 +1,7 @@
 public class maayan{
 	public static void main(String[] args){
 		for(int i=1;i<=5;i++){
-			System.out.println("Maayan Shabat");
+			System.out.println("Maayan Shabat 1234");
 		}
 	}
 }
